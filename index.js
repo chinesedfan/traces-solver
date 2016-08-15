1b865e3aab231f0180b97c660da4176f74c27c44
Xianming Zhong <chinesedfan@qq.com>
2016-08-15 13:34:59 +0800

    init project

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Main entry
+ *
+ * @param {Array} grid - contains NxN elements
+ * @return {Array} the solved grid, whose elements are {x: x, y: y}
+ */
+function solve(grid) {
+    var result = [];
+    return result;
+}
+
+module.exports = solve;
